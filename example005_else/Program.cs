2ca6cbc42ec1fbd2ebed4c9a3a27bc707783684f
Arina Egorova <egorova_aa_cheb@outlook.com>
2023-11-19 14:30:17 +0300

    add Example005

---
+++ example005_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+    Console.WriteLine("Hello, Super Maria!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
